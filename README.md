2c4e0de77c41745422668200f32ee56c52aa6d66
Heeringa <t.j.heeringa@utwente.nl>
2023-11-03 15:01:09 +0100

    Made full pipeline from conda to packages

--- README.md
+++ README.md
@@ -2,23 +2,11 @@
 
 This is a Python library to automatically retrieve .cff files from packages you have installed.
 
-**NB: This is a very early stage package.**
+**NB: This only works for packages installed through Anaconda.**
 
 ## How to use
-So far, you can find all the Github urls belonging to the packages installed through conda using
-```
-./conda_env_to_package_list.sh | sed "s/ /\n/g" | xargs -I {} python get_github_url.py {}
-```
-The command `conda_env_to_package_list.sh` takes you installed packages and gives you a space separated list of packages you have installed explicitly. `sed` with `xargs` feeds the packages one by one into the `get_github_url` command. This will go through the metadata and see if it can find a Github url.   
-
-## How it will work
 You can run the command
 ```
 python cfffind
 ```
-to go over all your installed Python packages, detect whether they are hosted on Github and, if so, check for a citation file. The citation files will be parsed into the `references` format specified by the `.cff` standard. By running
-```
-python cfffind >> references.bib
-```
-you can store the results in the file `references.bib`.
-
+to go over all your installed Python packages, detect whether they are hosted on Github and, if so, check for a citation file. The citation files will be parsed into the `references` format specified by the `.cff` standard, and written to `references.cff`.
--- cfffind.py
+++ cfffind.py
@@ -1,7 +1,11 @@
 from importlib import metadata
+import json
+from pathlib import Path
 import pkg_resources
 import re
 import requests
+import subprocess
+import yaml
 
 import click
 
@@ -10,11 +14,40 @@ GITHUB_URL_PATTERN = "https://github\.com/[^/]+/[^/,]+"
 
 
 @click.command()
-def cfffind():
-    installed_packages = [(d.project_name, d.version) for d in pkg_resources.working_set]
+@click.option('--output', '-o',
+              type=click.Path(writable=True),
+              default=Path("references.cff"),
+              help="File to write citations to.")
+@click.option('--summary/--no-summary', default=True)
+@click.option('--warnings/--no-warnings', default=True)
+def cfffind(output, summary, warnings):
+    dependencies_with_versions = json.loads(
+        subprocess.run(["conda", "env", "export", "--from-history", "--json"], text=True, capture_output=True).stdout
+    )["dependencies"]
+    dependencies = [dependency.split("=")[0] for dependency in dependencies_with_versions]
+
+    installed_packages = [d.project_name for d in pkg_resources.working_set]
+
+    not_found_dependencies = set(dependencies)
+    primary_packages = []
+    for package in installed_packages:
+        installer = metadata.distribution(package).read_text("INSTALLER")
+        if installer != "conda":
+            continue
+        try:
+            path_url = json.loads(metadata.distribution(package).read_text("direct_url.json"))["url"]
+            for dependency in not_found_dependencies:
+                if dependency in path_url:
+                    not_found_dependencies.remove(dependency)
+                    primary_packages.append(package)
+                    break
+        except TypeError:
+            if warnings:
+                click.echo(f"Info: Package {package} is installed through conda, but does not have a valid `direct_url.json` file.")
+
     github_urls = []
     no_github_packages = []
-    for (package, version) in installed_packages:
+    for package in installed_packages:
         package_metadata = metadata.metadata(package).json
 
         project_info = package_metadata.get('project_url', [])
@@ -44,8 +77,24 @@ def cfffind():
             for file in files:
                 if file["name"].startswith("CITATION"):
                     citation_urls.append(file["download_url"])
-                    click.echo(file["download_url"])
-    return citation_urls
+
+    citation_output_file = {"references": []}
+    for url in citation_urls:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            citation_yaml = yaml.safe_load(response.text)
+            citation_output_file["references"].append(citation_yaml)
+            click.echo(citation_yaml)
+
+    with open(output, "w", encoding="utf-8") as file:
+        yaml.dump(citation_output_file, file, allow_unicode=True)
+
+    if summary:
+        click.echo("Summary:")
+        click.echo(f"{len(primary_packages)} detected.")
+        click.echo(f"{len(github_urls)} of total {len(installed_packages)} packages with Github url.")
+        click.echo(f"{len(citation_urls)} of total {len(github_urls)} package with Github url has citation file.")
 
 
 if __name__ == "__main__":
